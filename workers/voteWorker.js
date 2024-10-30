031014fdb3c72d248a170463f4f49b3a7a6e24a4
Morten Ågotnes <morten.aagotnes@gmail.com>
2024-10-30 12:29:07 +0100

    Removed some comments

--- workers/voteWorker.js
+++ workers/voteWorker.js
@@ -18,13 +18,13 @@ async function workVotes() {
         await channel.assertQueue(queue, { durable: true });
         console.log('Waiting for messages in %s', queue);
 
-        // The callback here needs to be async
+
         channel.consume(queue, async (msg) => {
             try {
                 const voteData = JSON.parse(msg.content.toString());
                 console.log('Received vote:', voteData);
 
-                // Process the vote by saving it to MongoDB
+
                 const user = await User.findOne({ username: voteData.username });
                 if (!user) {
                     console.error('User not found:', voteData.username);
@@ -37,18 +37,18 @@ async function workVotes() {
                     return channel.nack(msg); // Reject the message but don’t requeue
                 }
 
-                // Create a new vote instance with references to user and voteOption
+
                 const newVote = new Vote({
                     publishedAt: voteData.publishedAt,
                     voteOption: voteOption._id, // Reference to voteOption
                     user: user._id, // Reference to user
                 });
 
-                // Save the new vote to the database
+
                 await newVote.save();
                 console.log('Vote saved to database', newVote);
 
-                // Acknowledge the message after successful processing
+
                 channel.ack(msg);
             } catch (error) {
                 console.error('Error processing or saving vote:', error);
@@ -60,5 +60,5 @@ async function workVotes() {
     }
 }
 
-// Start the worker
+
 workVotes();
